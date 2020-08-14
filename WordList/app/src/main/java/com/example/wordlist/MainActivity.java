f32428b294af838a3425bc7efb7455f0d8d12593
15174 <1517488035@qq.com>
2020-08-14 17:22:37 +0800

    program

---
+++ WordList/app/src/main/java/com/example/wordlist/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wordlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
